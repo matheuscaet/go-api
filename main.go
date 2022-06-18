cf24b5ce4801800038eda422a1f2709f68de3463
Matheus Caetano <matheuseprofissional@gmail.com>
2022-06-18 16:14:45 -0300

    feat: add routes configuration

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"go-api/configs"
+	"go-api/routes"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
-	})
+	//run database
+	configs.ConnectDB()
+
+	routes.UserRoute(app)
 
 	app.Listen(":6000")
 }
